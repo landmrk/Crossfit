53c4951ee2a87b98d9cb31cbe3e4393eb768bbd7
GiantPea <GiantPea@users.noreply.github.com>
2022-05-09 17:23:20 +0100

    map json

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,9 +2,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibGFuZG1yayIsImEiOiJjajV1dzZqemkxczA2MndxdHZrN
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/landmrk/ckcd73d8e7sca1imtabjdx8hr',
-  zoom: 8,
-  center: [-2.230155, 53.789061],
-  pitch: 1
+  zoom: 4,
+  pitch: 30
 });
 
 map.on('load', function() {
@@ -12,14 +11,23 @@ map.on('load', function() {
   // from GL JS's use in the added source. You can use any request method (library
   // or otherwise) that you want.
   d3.json(
-    'https://zq3ltesiy7.execute-api.us-east-1.amazonaws.com/dev/getLocations',
+    'https://sheets.googleapis.com/v4/spreadsheets/1opFPe_w7wU2PMkAWtsvbNWHvZtxzMjM7l2pxne3Qswc/values/Locations?alt=json&key=AIzaSyAxoum5Sl8ZK88X8Yjj6LgLi6qV2Dgb_dA',
     function(err, data) {
 
       const { route,
       reachedRoute,
-      mapFacts,
+      challengeMarkers,
+      infoMarkers,
+      covidMarkers,
       metadata } = data;
       if (err) throw err;
+
+      // set metadata
+      localStorage.setItem('metadata', metadata);
+      const { daysRemaining, justGiving, distanceRan } = metadata;
+      document.getElementById('daysRemaining').innerHTML = daysRemaining;
+      document.getElementById('justGiving').innerHTML = justGiving;
+      document.getElementById('distanceRan').innerHTML = distanceRan;
 
       // save full coordinate list for later
       const { coordinates } = reachedRoute.features[0].geometry;
@@ -48,9 +56,9 @@ map.on('load', function() {
           'line-join': 'round'
         },
         'paint': {
-          'line-color': '#fff',
-          'line-opacity': 0.8,
-          'line-width': 6        }
+          'line-color': '#FBDC01',
+          'line-opacity': 1,
+          'line-width': 8        }
       });
       // add it to the map
       map.addSource('trace', {
@@ -67,17 +75,17 @@ map.on('load', function() {
           'line-join': 'round'
         },
         'paint': {
-          'line-color': '#50C9A4',
+          'line-color': '#E5321E',
           'line-opacity': 1,
-          'line-width': 6        }
+          'line-width': 8        }
       });
 
       // setup the viewport
       map.jumpTo({
-        'center': routeInit[1],
-        'zoom': 8
+        'center': routeInit[0],
+        'zoom': 4
       });
-      map.setPitch(1);
+      map.setPitch(30);
 
       //on a regular basis, add more coordinates from the saved list and update the map
       var i = 0;
@@ -93,28 +101,38 @@ map.on('load', function() {
         } else {
           window.clearInterval(timer);
         }
-      }, 2500);
+      }, 300);
 
-      map.loadImage('../assets/imgs/hotspot.png', (error, image) => {
+      map.loadImage('../assets/imgs/hotspot-info.png', (error, image) => {
         if (error) throw error;
         map.addImage('info', image);
       });
+      map.loadImage('../assets/imgs/hotspot-ball.png', (error, image) => {
+        if (error) throw error;
+        map.addImage('ball', image);
+      });
+      map.loadImage('../assets/imgs/hotspot-fact.png', (error, image) => {
+        if (error) throw error;
+        map.addImage('fact', image);
+      });
 
-      setmapFacts(mapFacts)
+      setChallengeMarkers(challengeMarkers)
+      setInfoMarkers(infoMarkers)
+      setCovidMarkers(covidMarkers)
 
       // Add zoom and rotation controls to the map.
       map.addControl(new mapboxgl.NavigationControl());
     });
     }
   );
-function setmapFacts(mapFacts) {
+function setChallengeMarkers(challengeMarkers) {
       map.addSource('places', {
         'type': 'geojson',
-        'data': mapFacts,
+        'data': challengeMarkers,
         'cluster': false
       });
 
-      map.loadImage('../assets/imgs/hotspot.png', (error, image) => {
+      map.loadImage('../assets/imgs/hotspot-ball.png', (error, image) => {
         if (error) throw error;
         map.addImage('hotspot', image);
       });
@@ -138,8 +156,8 @@ function setmapFacts(mapFacts) {
         var coordinates = e.features[0].geometry.coordinates.slice();
         //var description = e.features[0].properties.description;
 
-        const { figure, icon, poi, location } = e.features[0].properties;
-        const copy = `<h4 style="margin-bottom:5px">Location</h4> <p>${location}</p> <h4 style="margin-bottom:5px">Tour Date</h4> <p>${poi}</p>`;
+        const { description, image } = e.features[0].properties;
+        const copy = `<img src="${image}" width="140" /> <p>${description}</p> <strong>#12Challenges</strong>`;
 
         // Ensure that if the map is zoomed out such that multiple
         // copies of the feature are visible, the popup appears
@@ -161,6 +179,120 @@ function setmapFacts(mapFacts) {
 
       // Change it back to a pointer when it leaves.
       map.on('mouseleave', 'places', function() {
+        map.getCanvas().style.cursor = '';
+      });
+}
+
+function setInfoMarkers(infoMarkers) {
+      map.addSource('infoMarkers', {
+        'type': 'geojson',
+        'data': infoMarkers,
+        'cluster': false
+      });
+
+      map.loadImage('../assets/imgs/hotspot-info.png', (error, image) => {
+        if (error) throw error;
+        map.addImage('hotspot2', image);
+      });
+
+      // Add a layer showing the places.
+      map.addLayer({
+        'id': 'infoMarkers',
+        'type': 'symbol',
+        'source': 'infoMarkers',
+        'layout': {
+          'icon-image': 'hotspot2',
+          'icon-allow-overlap': true,
+          'icon-size': 0.5
+        }
+      });
+
+      // When a click event occurs on a feature in the places layer, open a popup at the
+      // location of the feature, with description HTML from its properties.
+      map.on('click', 'infoMarkers', function(e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        //var description = e.features[0].properties.description;
+
+        const { heading1, heading2, description } = e.features[0].properties;
+        const copy = `${heading1} <strong>${heading2}</strong> <p>${description}</p>`;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(copy)
+          .addTo(map);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on('mouseenter', 'infoMarkers', function() {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on('mouseleave', 'infoMarkers', function() {
+        map.getCanvas().style.cursor = '';
+      });
+}
+
+function setCovidMarkers(covidMarkers) {
+      map.addSource('covidMarkers', {
+        'type': 'geojson',
+        'data': covidMarkers,
+        'cluster': false
+      });
+
+      map.loadImage('../assets/imgs/hotspot-fact.png', (error, image) => {
+        if (error) throw error;
+        map.addImage('hotspot3', image);
+      });
+
+      // Add a layer showing the places.
+      map.addLayer({
+        'id': 'covidMarkers',
+        'type': 'symbol',
+        'source': 'covidMarkers',
+        'layout': {
+          'icon-image': 'hotspot3',
+          'icon-allow-overlap': true,
+          'icon-size': 0.5
+        }
+      });
+
+      // When a click event occurs on a feature in the places layer, open a popup at the
+      // location of the feature, with description HTML from its properties.
+      map.on('click', 'covidMarkers', function(e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        //var description = e.features[0].properties.description1;
+
+        const { description1, description2 } = e.features[0].properties;
+        const description = `<p>${description1}</p> <p>${description2}</p>`;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on('mouseenter', 'covidMarkers', function() {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on('mouseleave', 'covidMarkers', function() {
         map.getCanvas().style.cursor = '';
       });
 }
